fbaddf150bba9c0fb90c175631a9545265f33a9e
Romulo Tavares <romulo.tavares@corp.globo.com>
2012-10-18 16:19:21 -0300

    Adicionado bootstrap no projeto

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20121018185519) do
+
+  create_table "pessoas", :force => true do |t|
+    t.string   "nome"
+    t.string   "descricao"
+    t.string   "imagem"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
