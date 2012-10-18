436ca668a86704f42068173d07da0589b70af529
Romulo Tavares <romulo.tavares@corp.globo.com>
2012-10-18 15:26:18 -0300

    Iniciando projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DueloTempoReal::Application.config.secret_token = '408371e5ddeaa1596800627c66b75905a9e8791e8fa5a9da3e783cee5ed32ee4a17e150dcaab70f3cf656b34a426af7763b6d0ba102d98db83ee84ad40d4533f'
