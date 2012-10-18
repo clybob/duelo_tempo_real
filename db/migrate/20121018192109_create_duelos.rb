8900ac752a77c26721faeb558385b6a6c52538c0
sergiojunior <sergio.junior@corp.globo.com>
2012-10-18 16:37:37 -0300

    Retirado campo pessoa da migrate de duelos

--- db/migrate/20121018192109_create_duelos.rb
+++ db/migrate/20121018192109_create_duelos.rb
@@ -1,7 +1,6 @@
 class CreateDuelos < ActiveRecord::Migration
   def change
     create_table :duelos do |t|
-      t.string :pessoa
       t.datetime :data_inicio
       t.datetime :data_fim
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121018185519) do
+ActiveRecord::Schema.define(:version => 20121018192109) do
+
+  create_table "duelos", :force => true do |t|
+    t.string   "pessoa"
+    t.datetime "data_inicio"
+    t.datetime "data_fim"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "pessoas", :force => true do |t|
     t.string   "nome"
