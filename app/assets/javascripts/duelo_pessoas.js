cd26cfaffe5f37d7e892498e76b9a54486af8666
Romulo Tavares <romulo.tavares@corp.globo.com>
2012-10-18 21:55:19 -0300

    Criada estrutura client basica pra duelo

--- app/assets/javascripts/duelo_pessoas.js
+++ app/assets/javascripts/duelo_pessoas.js
@@ -1,2 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function(){
+    $('.btn-voto').on('click', function() {
+        var dueloPessoaId = $(this).attr('data-duelo-pessoa-id');
+        var url = '/duelo_pessoas/' + dueloPessoaId;
+        var dueloId = $(this).attr('data-duelo-id');
+        var participanteId = $(this).attr('data-participante-id');
+        var dados = {
+            "duelo": dueloId,
+            "pessoa": participanteId,
+            "votos": "+1"
+        };
+        
+        $.post(url, dados);
+    });
+});
